e6dec8347ba9c4c09606fd4e2fff044603397c64
usmanajibola <usmanajibolaabass@gmail.com>
2020-10-27 04:08:23 +0100

    worked on the reusable input component and used reactive forms. Next push will include form submission handling

--- js-form/src/app/form/form.component.ts
+++ js-form/src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-form',
@@ -7,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  form = new FormGroup({
+    name: new FormControl('', [
+      Validators.required, 
+      ]),
+    date: new FormControl('', [
+      Validators.required
+    ]),
+    bribeAmount: new FormControl('', [
+      Validators.required
+    ])
+  })
   constructor() { }
 
   ngOnInit(): void {
---
+++ js-form/src/app/input/input.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+
+  @Input() control:any;
+  @Input() label:string;
+  @Input() type:string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
